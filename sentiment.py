7d78b57f43b7ffdfa881434b6f750ee2f0b42953
Amy Shen <amyshen@gmail.com>
2017-11-14 13:53:18 -0800

    Add function to create matplotlib graph of average sentiment over time

--- sentiment.py
+++ sentiment.py
@@ -4,6 +4,10 @@ import os
 import json
 import sys
 import twitter
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
+import time
 
 from afinn import Afinn
 from urllib.parse import urlencode
@@ -41,6 +45,37 @@ def apply_sentiment(tweet):
     tweet['vader_sentiment'] = vader_analyzer.polarity_scores(tweet['text'])
     return tweet
 
+# Convert Twitter's 'created_at' timestamp into a datetime object
+def parse_timestamp(ts):
+    return datetime.fromtimestamp(time.mktime(time.strptime(ts,'%a %b %d %H:%M:%S +0000 %Y')))
+
+# Return the average of the afinn and vader sentiment scores for a tweet
+def avg_sentiment(tweet):
+  return (tweet['afinn_sentiment'] + tweet['vader_sentiment']['compound']) / 2.0
+
+# Creates a histogram of the results and saves it in output_file.
+def create_histogram(results, output_file):
+    fig, ax = plt.subplots(1,1)
+    dates = [parse_timestamp(tweet['created_at']) for tweet in results]
+    sentiment_vals = [avg_sentiment(tweet) for tweet in results]
+
+    months = mdates.MonthLocator()  # every month
+    days = mdates.DayLocator()  # every day of the month
+
+    ax.xaxis.set_major_locator(months)
+    ax.xaxis.set_minor_locator(days)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%y'))
+
+    datemin = min(dates)
+    datemax = max(dates)
+    ax.set_xlim(datemin, datemax)
+
+    ax.plot(dates, sentiment_vals)
+    ax.xaxis_date()
+    plt.xlabel('Date')
+    plt.ylabel('Average sentiment score')
+    plt.show()
+    plt.savefig(output_file)
 
 def process_summary(results):
     """
@@ -49,6 +84,13 @@ def process_summary(results):
     - average scores over time
     - average scores grouped by location
     - histogram of frequency distribution for each sentiment score
+
+    @TODO add more stats:
+    - median
+    - stdev
+    - modality summary
+    - most frequent terms used in these tweets
+    - most common timezones from which people tweeted?
     """
 
     afinn_scores = [tweet['afinn_sentiment'] for tweet in results]
